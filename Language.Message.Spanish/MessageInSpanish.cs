cff99d5e655b150fa8490df8cc77ac202a174d7f
Milton Arteaga <Milton.Arteaga@openintl.com>
2025-06-03 18:30:32 -0500

    Add new proyect to support spanish language

---
+++ Language.Message.Spanish/MessageInSpanish.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Language.Message.Spanish
+{
+    public class MessageInSpanish : Interface.IMessage
+    {
+        public string Description
+        {
+            get { return "Spanish"; }
+        }
+
+        public string Message
+        {
+            get { return "Hola"; }
+        }
+    }
+}
